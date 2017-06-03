d27365ae8944730cda6d85576341a61e53a33aeb
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-06-03 17:58:55 +0200

    load the gloabl descriptor table from stage2

--- boot/stage2.asm
+++ boot/stage2.asm
@@ -5,6 +5,108 @@ org 0x0
 ; machine is executing real mode (16 bits) mode (in 80x86 architecture)
 bits 16
 
-cli
+; Loads the Global Descriptor Table (null, code and data descriptors)
+; every descriptor is 64 bits long
 
+jmp end     ; skip the GDT data part
+
+; -----------------------------------------------------------------
+; Global descriptor table
+; -----------------------------------------------------------------
+
+; bits 0-15: bits 0 - 15 of the segment limit
+; bits 16-39: bits 0 - 23 of the base address
+; bit 40: access bit for virtual memory, 0 to ignore virtual memory
+; bit 41: 1 (read only for data segments, execute only for code segments),
+;         0 (read and write data segments, read and execute code segments)
+; bit 42: expension direction bit, 0 to ignore
+; bit 43: descriptor type (0: data, 1: code)
+; bit 44: descriptor bit (0: system descriptor, 1: code or data descriptor)
+; bits 45-46: ring of the descriptor (from 0 to 3)
+; bit 47: indicates if the segment uses virtual memory (0: no, 1: yes)
+; bits 48-51: bits 16-19 of the segment limit
+; bit 52-53: OS reserved, set to 0
+; bit 54: 0 (16 bits segment), 1 (32 bits segment)
+; bit 55: granulariry bit
+;         0 (the limit is in 1 byte blocks)
+;         1 (the limit is in 4 Kbytes blocks)
+;         if set to 1, the limit becomes {limit}*4096
+; bits 56-63: bits 24 - 32 of the base address
+
+gdt_start:
+
+; -----------------------------------------------------------------
+; null descriptor (only 0)
+; -----------------------------------------------------------------
+dd 0
+dd 0
+
+; -----------------------------------------------------------------
+; code descriptor (code can be stored from 0x0 to 0xFFFFF)
+; -----------------------------------------------------------------
+dw 0xFFFF       ; segment limit bits 0-15 is 0xFFFF
+dw 0            ; segment base is 0x0
+db 0
+
+; 0: do not handle virtual memory
+; 1: the code segments can be read and executed
+; 0: expension direction ignored
+; 1: code descriptor
+; 1: code/data descriptor, not system descriptor
+; 00: the segments are executed at ring 0
+; 0: the segments do not use virtual memory
+db 00011010b
+
+; 1111: segment limit bits 0-15 is 0xFFFF, complete segment limit address is now 0xFFFFF
+; 00: OS reserved, set to 0
+; 1: 32 bits segment
+; 1: enable granularity, the limit is now 0xFFFFF * 4096 = 0xFFFFF000 (4 Gbytes)
+db 11001111b
+
+db 0            ; segment base is 0x0
+
+; -----------------------------------------------------------------
+; data descriptor (code can be stored from 0x0 to 0xFFFFF)
+; -----------------------------------------------------------------
+dw 0xFFFF       ; segment limit bits 0-15 is 0xFFFF
+dw 0            ; segment base is 0x0
+db 0
+
+; 0: do not handle virtual memory
+; 1: the data segments can be read and write
+; 0: expension direction ignored
+; 0: data descriptor
+; 1: code/data descriptor, not system descriptor
+; 00: the segments are executed at ring 0
+; 0: the segments do not use virtual memory
+db 00010010b
+
+; 1111: segment limit bits 0-15 is 0xFFFF, complete segment limit address is now 0xFFFFF
+; 00: OS reserved, set to 0
+; 1: 32 bits segment
+; 1: enable granularity, the limit is now 0xFFFFF * 4096 = 0xFFFFF000 (4 Gbytes)
+db 11001111b
+
+db 0            ; segment base is 0x0
+
+; -----------------------------------------------------------------
+; end of the GDT
+; -----------------------------------------------------------------
+
+gdt_end:
+
+    ; the location that stores the value to load with LGDT
+    ; must be in the format:
+    ; bits 0 - 15: GDT size
+    ; bits 16 - 47: GDT starting address
+
+    dw gdt_end - gdt_start - 1      ; the size of the GDT
+    dd gdt_start                    ; the starting address of the GDT
+
+end:
+
+    ; load the GDT into GDTR register
+    lgdt [gdt_end]
+
+cli
 hlt
