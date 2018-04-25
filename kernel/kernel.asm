a59f6b9b594c8fe96a24540e6cc67662f04456ca
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-04-25 20:41:44 +0200

    include file to load idt from the kernel

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -3,6 +3,22 @@ global _start
 section .text
 bits 32
 
+; NOTE: the file format is ELF (but only for compilation and linking process),
+; in fact, the calling code does not know about ELF and starting points at all,
+; from this point of view, the kernel is still a raw binary file,
+; so we have to explicitly jump to the beginning
+jmp _start
+
+;-----------------------------------------------------------------------------
+; Inclusions
+;-----------------------------------------------------------------------------
+
+%include 'idt.asm'   ; IDT routines
+
+;-----------------------------------------------------------------------------
+; Kernel
+;-----------------------------------------------------------------------------
+
 _start:
 
     ; ensure data segment, stack segment and extra segment
